c0b16fce8c282db4dfe525e032f468f88e38c5d6
anbbch <155988779+anbbch@users.noreply.github.com>
2026-04-01 15:16:12 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,3 +14,18 @@ variable "image_url" {
 variable "image_tag" {
   type = string
 }
+
+variable "github_actor" {
+  description = "GitHub username"
+  type        = string
+}
+
+variable "db_url" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_host" {
+  type        = string
+  description = "PostgreSQL hostname interne Render"
+}
