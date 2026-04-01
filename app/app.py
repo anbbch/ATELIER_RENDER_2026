16e38417093e899a319d5978e2df7167fe27ed34
anbbch <155988779+anbbch@users.noreply.github.com>
2026-04-01 15:17:34 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,6 +3,9 @@ import os
 
 app = Flask(__name__)
 
+def get_db():
+    return psycopg2.connect(os.environ.get("DATABASE_URL"))
+
 @app.route("/")
 def home():
     return "Flask + Docker + GHCR + Terraform + Render"
@@ -22,6 +25,27 @@ def info():
 @app.route("/env")
 def env():
     return {"env": os.getenv("ENV")}
+
+@app.route("/users")
+def users():
+    try:
+        conn = get_db()
+        cur = conn.cursor()
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                name TEXT
+            );
+        """)
+        cur.execute("INSERT INTO users (name) VALUES ('BOUBCHIR_Anya');")
+        conn.commit()
+        cur.execute("SELECT * FROM users;")
+        rows = cur.fetchall()
+        cur.close()
+        conn.close()
+        return jsonify(rows)
+    except Exception as e:
+        return {"error": str(e)}, 500
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
