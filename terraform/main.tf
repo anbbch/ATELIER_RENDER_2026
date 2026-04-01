e6b63be58a3af87cb475449eaeb47ca001ec45f0
anbbch <155988779+anbbch@users.noreply.github.com>
2026-04-01 15:14:44 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,11 +12,7 @@ provider "render" {
   owner_id = var.render_owner_id
 }
 
-variable "github_actor" {
-  description = "GitHub username"
-  type        = string
-}
-
+# --- Flask Web Service ---
 resource "render_web_service" "flask_app" {
   name   = "flask-render-iac-${var.github_actor}"
   plan   = "free"
@@ -28,9 +24,33 @@ resource "render_web_service" "flask_app" {
       tag       = var.image_tag
     }
   }
+
   env_vars = {
     ENV = {
       value = "production"
+    }
+    DATABASE_URL = {
+      value = var.db_url
+    }
+  }
+}
+
+# --- Adminer Web Service ---
+resource "render_web_service" "adminer" {
+  name   = "adminer-${var.github_actor}"
+  plan   = "free"
+  region = "frankfurt"
+
+  runtime_source = {
+    image = {
+      image_url = "ghcr.io/bstocker/adminer-render"
+      tag       = "latest"
+    }
+  }
+
+  env_vars = {
+    ADMINER_DEFAULT_SERVER = {
+      value = var.db_host
     }
   }
 }
